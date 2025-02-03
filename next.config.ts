2c8b639dd80658ae0a030c7bf32ef9f3584e83d0
usmankhan002011 <usmankhan002011@gmail.com>
2025-02-03 23:09:59 +0500

    config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // Enable static export
-  images: { unoptimized: true }, // Fix for next/image in static builds
+  images: { unoptimized: true }, // Fix for next/image
+  basePath: "/maths", // Must match your repo name
+  assetPrefix: "/maths/", // Ensures assets load correctly
 };
 
 export default nextConfig;
